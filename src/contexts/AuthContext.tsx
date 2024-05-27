a52ae8a33c80c7080a4a7ba92722536afcd91466
rafaelsoutoo <rafaelsouto70017@gmail.com>
2024-05-27 01:43:19 -0300

    add files

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,29 @@
+import { createContext, ReactNode } from "react";
+import { api } from '@services/api';
+
+export type AuthContextDataProps = {
+  signIn: (email: string, password: string) => Promise<void>;
+}
+
+type AuthContextProviderProps = {
+  children: ReactNode
+}
+
+export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  async function signIn(email: string, password: string) {
+    try {
+      const { data } = await api.post('/sessions', { email, password });
+      console.log(data);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
---
+++ src/storage/storageConfig.ts
@@ -0,0 +1,4 @@
+const USER_STORAGE = '@revive:user';
+const AUTH_STORAGE = '@revive:token';
+
+export { USER_STORAGE, AUTH_STORAGE  }
